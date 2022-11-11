4fa317bccd88dd7af2826710422d8162d510c47f
gilangsup <gilangsprb@gmail.com>
2022-11-11 08:36:45 +0700

    added problem 2

---
+++ problem2.js
@@ -0,0 +1,15 @@
+const fibonacci_series =  (n) =>
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    let s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+ console.log(fibonacci_series(8));
